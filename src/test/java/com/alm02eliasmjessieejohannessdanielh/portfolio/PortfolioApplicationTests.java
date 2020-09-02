8c2fd41d4d01d19c38f6c0c86fdf60f5bd7f2ffc
EliasMehr <elias.mehr@gmail.com>
2020-09-02 14:52:32 +0200

    initial setup

---
+++ src/test/java/com/alm02eliasmjessieejohannessdanielh/portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alm02eliasmjessieejohannessdanielh.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortfolioApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
